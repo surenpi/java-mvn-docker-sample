5ae1d2352624858ccd587d09e4d68cd5bf93565a
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-04-23 15:10:52 +0800

    Add demo for junit case

---
+++ src/test/java/com/surenpi/test/demo/JustForDemo.java
@@ -0,0 +1,18 @@
+package com.surenpi.test.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class JustForDemo {
+    @Test
+    public void one() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void two() {
+        assertEquals("", "");
+    }
+}

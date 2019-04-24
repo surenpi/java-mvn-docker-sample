43b6f08109dec7e1ba42a055681fb702bafb7a68
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-04-24 12:02:18 +0800

    Junit test class should always end with Test

--- src/test/java/com/surenpi/test/demo/JustForDemoTest.java
+++ src/test/java/com/surenpi/test/demo/JustForDemoTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class JustForDemo {
+public class JustForDemoTest {
     @Test
     public void one() {
         assertTrue(true);
